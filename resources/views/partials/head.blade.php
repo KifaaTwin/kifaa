5ef91c97a83835391b38f848f6cd958f02ae27c4
wajennaif <wooj7@outlook.com>
2026-05-28 05:36:24 +0300

    Remove responsive CSS from Vite

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -57,11 +57,7 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 
-@vite([
-    'resources/css/app.css',
-    'resources/css/responsive.css',
-    'resources/js/app.js'
-])@fluxAppearance
+@vite(['resources/css/app.css', 'resources/js/app.js'])@fluxAppearance
 
 <script>
     function getStoredKifaaTheme() {
